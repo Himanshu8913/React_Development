177a61d8bd6be997da07a9fb5be4c77987f5f179
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-02 23:51:23 +0530

    added scripts and render using initial jsx

--- Basic/App.js
+++ Basic/App.js
@@ -1,26 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * 
-<div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
-* ReactElement(Object) => HTML(Browser Understand)
-*/
-const parent = React.createElement("div" ,{id: "parent"},
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")
-    ]
-    )
-);
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World!!!");
+// React Element
+const jsxHeading = <h1 id="heading">Heading using JSX 🎉</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(parent);
+root.render(jsxHeading);
