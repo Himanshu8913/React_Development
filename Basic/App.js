4f334243b225861b482e55b1bbe51b3e3b6f9ce6
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-03 00:20:57 +0530

    adding functional components

--- Basic/App.js
+++ Basic/App.js
@@ -2,8 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-const jsxHeading = <h1 id="heading">Heading using JSX 🎉</h1>
+// JSX(transpiled before it reaches the JS) - PARCEL -> Babel
+// JSX(convert by Babel) => React.createElement => ReactElement - JS Object => HTMLElement(render)
+const jsxHeading = <h1 id="heading">Heading using JSX 🎉</h1>;
+
+// React Functional Component
+const Title = () => (
+  // as its returning 1 liner so no need to add return keyword.
+    <h1>This is Title Component</h1>
+);
+
+const HeadingComponent = () => {
+    return (
+    <div>
+        <Title />
+        <h1 id="heading">This is h1 tag in functional component.</h1>
+    </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
