9227d411112f7060b750652f39855b8b2c49590b
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-04 01:27:03 +0530

    created 1 card using swiggy api json data

--- Project-1-Food_Ordering_App/App.js
+++ Project-1-Food_Ordering_App/App.js
@@ -21,16 +21,86 @@ const Header = () => {
 }
 
 const RestaurantCard = (props) => {  // props = {resName, cuisines, rating, deliveryTime} => destructuring
-    const {resName, cuisines, rating, deliveryTime} = props;
+    const { resData } = props;
     return (
         <div className="res-card">
-            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/f01666ac73626461d7455d9c24005cd4" />
-            <h3>{resName}</h3>
-            <h4>{cuisines}</h4>
-            <h4>{rating} stars</h4>
-            <h4>{deliveryTime} min</h4>
+            <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/" + resData.info.cloudinaryImageId } />
+            <h3>{resData.info.name}</h3>
+            <h4>{(resData.info.cuisines).join(", ")}</h4>
+            <h4>{resData.info.avgRatingString} stars</h4>
+            <h4>{resData.info.costForTwo}</h4>
+            <h4>{resData.info.sla.deliveryTime} min</h4>
         </div>
     )
+}
+
+const resObj = {
+    "info": {
+        "id": "84400",
+        "name": "Burger King",
+        "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "locality": "Maharana Pratap Nagar",
+        "areaName": "Maharana Pratap Nagar",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+            "Burgers",
+            "American"
+        ],
+        "avgRating": 4.1,
+        "parentId": "166",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "10K+",
+        "sla": {
+            "deliveryTime": 44,
+            "lastMileTravel": 2.2,
+            "serviceability": "SERVICEABLE",
+            "slaString": "40-45 mins",
+            "lastMileTravelString": "2.2 km",
+            "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+            "nextCloseTime": "2024-03-04 03:00:00",
+            "opened": true
+        },
+        "badges": {},
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+            "entityBadges": {
+                "imageBased": {},
+                "textBased": {},
+                "textExtendedBadges": {}
+            }
+        },
+        "aggregatedDiscountInfoV3": {
+            "header": "60% OFF",
+            "subHeader": "UPTO ₹120"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+            "logoCtx": {
+                "text": "BENEFITS",
+                "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+            },
+            "freedelMessage": "FREE DELIVERY"
+        },
+        "orderabilityCommunication": {
+            "title": {},
+            "subTitle": {},
+            "message": {},
+            "customIcon": {}
+        },
+        "differentiatedUi": {
+            "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            "differentiatedUiMediaDetails": {
+                "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                "lottie": {},
+                "video": {}
+            }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {}
+    },
 }
 
 const Body = () => {
@@ -41,8 +111,7 @@ const Body = () => {
             </div>
 
             <div className="res-container">
-                <RestaurantCard resName="KFC" cuisines="North Indian, Food Bucket" rating="4.4" deliveryTime="38" />
-                <RestaurantCard resName="Burger King" cuisines="Burger, Fries, Fast Food" rating="4.2" deliveryTime="22" />
+                <RestaurantCard resData={resObj} />
             </div>
         </div>
     )
