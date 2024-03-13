35d18cc178baafc467f598baed7f802a873a645c
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-13 12:05:20 +0530

    using state in class based components

--- Project-1-Food_Ordering_App/src/components/User.js
+++ Project-1-Food_Ordering_App/src/components/User.js
@@ -7,14 +7,29 @@ class User extends React.Component {
     constructor(props) {
         super(props);
 
-        console.log(props);
+        this.state = {
+            count: 0,
+            count2: 0,
+        }
+        // console.log(props);
     }
     render() {
-        
-        const {name, location, contact} = this.props;
+
+        const { name, location, contact } = this.props;
+        const { count, count2 } = this.state;
 
         return (
             <div className="user-card">
+                <h2>Count: {count}</h2>
+                <h2>Count2: {count2}</h2>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1,
+                        count2: this.state.count2 + 2,
+                    })
+                }}>
+                    Count Increase
+                </button>
                 <h2>Name: {name}</h2>
                 <h4>Location: {location}</h4>
                 <h4>Contact: {contact}</h4>
