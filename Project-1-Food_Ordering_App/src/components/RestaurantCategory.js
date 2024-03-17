4497f5f8ab9b7ebe2de18bf1cb0b3c85cc7a6e6f
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-18 02:39:59 +0530

    fixed props due to api issues

--- Project-1-Food_Ordering_App/src/components/RestaurantCategory.js
+++ Project-1-Food_Ordering_App/src/components/RestaurantCategory.js
@@ -8,11 +8,11 @@ export const RestaurantCategory = ({ data }) => {
             <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4">
                 <div className="flex justify-between">
                     <span className="font-bold text-lg">
-                        {data.card.title} ({data.card.categories.length})
+                    {data.title} ({data.itemCards.length})
                     </span>
                     <span>⬇️</span>
                 </div>
-                <ItemList items={data.card.categories} />
+                <ItemList items={data.itemCards} />
             </div>
         </div>
     );
--- Project-1-Food_Ordering_App/src/components/RestaurantMenu.js
+++ Project-1-Food_Ordering_App/src/components/RestaurantMenu.js
@@ -11,17 +11,17 @@ const RestaurantMenu = () => {
     const resInfo = useRestaurantMenu(id);
 
     if (resInfo === null) return <Shimmer />;
+    console.log(resInfo);
 
     const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
-    // console.log(resInfo?.cards[0]?.card?.card?.info);
-    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
     // console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
 
-    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
-        (c) => (
-            c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.NestedItemCategory"
-        )
-    );
+    const categories =
+        resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+            (c) =>
+                c.card?.["card"]?.["@type"] ===
+                "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        );
     // console.log(categories);
     return (
         <div className="text-center">
@@ -32,7 +32,10 @@ const RestaurantMenu = () => {
             {/* categories accordians */}
             {categories.map(
                 (category) => (
-                    <RestaurantCategory data={category?.card} />
+                    <RestaurantCategory
+                        key={category?.card?.card.title}
+                        data={category?.card?.card}
+                    />
                 )
             )}
         </div>
