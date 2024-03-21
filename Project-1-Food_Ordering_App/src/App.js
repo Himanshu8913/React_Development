7873d701c921306d4090d673b666ff6aa3a1cdd6
Himanshu Gupta <himanshugupta558@gmail.com>
2024-03-22 03:02:37 +0530

    making cart working using redux toolkit

--- Project-1-Food_Ordering_App/src/App.js
+++ Project-1-Food_Ordering_App/src/App.js
@@ -8,6 +8,8 @@ import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const Grocery = lazy(() => import("./components/Grocery"));
 
@@ -25,12 +27,15 @@ const AppLayout = () => {
         setUserName(data.name);
     }, []);
     return (
-        <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-            <div className="app">
-                <Header />
-                <Outlet />
-            </div>
-        </UserContext.Provider>
+        <Provider store={appStore}>
+            <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+                <div className="app">
+                    <Header />
+                    <Outlet />
+                </div>
+            </UserContext.Provider>
+        </Provider>
+
     )
 }
 
--- Project-1-Food_Ordering_App/src/components/Header.js
+++ Project-1-Food_Ordering_App/src/components/Header.js
@@ -3,6 +3,7 @@ import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 
 const Header = () => {
 
@@ -12,6 +13,10 @@ const Header = () => {
 
     const { loggedInUser } = useContext(UserContext);
     console.log(loggedInUser);
+
+    // Subscribing to the store using selector
+    const cartItems = useSelector((store) => store.cart.items);
+    console.log(cartItems);
 
     return (
         <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
@@ -34,7 +39,7 @@ const Header = () => {
                     <li className="px-4">
                         <Link to="/grocery">Grocery</Link>
                     </li>
-                    <li className="px-4">Cart</li>
+                    <li className="px-4 font-bold text-lg">Cart - ({cartItems.length} items)</li>
                     <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-1.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={() => {
                         // btnName = "Logout";
                         btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
